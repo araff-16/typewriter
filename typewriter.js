ae14cba3f300ebdd214418c694be95a934c45ef0
araff-16 <alex_raffa16@hotmail.com>
2021-11-02 14:05:51 +0000

    syntaxt

--- typewriter.js
+++ typewriter.js
@@ -1,14 +1,14 @@
-const sentence = "hello there from lighthouse labs"
+const sentence = "hello there from lighthouse labs";
 
-let wait = 0
+let wait = 0;
 for (const char of sentence) {
   //process.stdout.write(char)
   setTimeout(() => {
     process.stdout.write(char);
-  }, wait)
-  wait += 70
+  }, wait);
+  wait += 70;
 }
 
 setTimeout(() => {
   console.log('\n');
-}, wait)
+}, wait);
